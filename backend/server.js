4b3639ff879ec12ae7963f01726dab106fa07df8
Imraj <imrajrabbani2003@gmail.com>
2025-04-13 12:21:15 +0600

    feat: adding auth routes to server file

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const authRoutes = require('./routes/authRoutes');
 
 app.use(cors());
 app.use(express.json());
