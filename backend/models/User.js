7dce91a3e8b154d3acd1fd6302d6137e308c7eff
Imraj <imrajrabbani2003@gmail.com>
2025-04-13 11:57:47 +0600

    feat: created the model file for user

---
+++ backend/models/User.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+  },
+  role: {
+    type: String,
+    enum: ['donor', 'volunteer', 'admin'],
+    default: 'donor'
+  },
+  location: {
+    type: String
+  },
+
+}, { timestamps: true });
+
+// Middleware to hash the password before saving
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare entered password with the stored hashed password
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = mongoose.model('User', UserSchema);
