c5c8c6d853afa683ba164b94ea165cef3ac1cb1a
Imraj <imrajrabbani2003@gmail.com>
2025-04-14 08:24:13 +0600

    fix: added a missing a line in the server.js fille

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,28 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const authRoutes = require('./routes/authRoutes');
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URI;
-mongoose.connect(uri);
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('MongoDB database connection established successfully');
-});
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('MongoDB database connected successfully');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    });
+
+app.use("/", authRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
